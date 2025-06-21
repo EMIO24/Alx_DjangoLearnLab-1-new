018eefd8a98d3c312c03b772c17512a555c95cfa
EMIO24 <emmanuel.osarodion@icloud.com>
2025-06-21 18:33:42 +0100

    feat: Set up initial Django project and API app

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import ExampleModel  # Import your model
-
-class ExampleForm(forms.ModelForm):
-    class Meta:
-        model = ExampleModel
-        fields = ['name', 'email']  # Include the fields you need
-
